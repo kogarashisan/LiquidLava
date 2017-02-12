a56c6be9da92dc0db2c4c40d8966b92a42a1b649
kogarashisan <kogarashisan@users.noreply.github.com>
2017-02-12 14:42:26 +0200

    next version - work-in-progress

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,8 @@ files_list = [
 ]);
 
 module.exports = function(config) {
-    config.set({
+
+    var configuration = {
         // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
         frameworks: ['mocha', 'chai-spies', 'chai' /*, 'browserify'*/],
@@ -45,6 +46,12 @@ module.exports = function(config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['Chrome'/*, 'Firefox', 'PhantomJS', 'IE', 'Safari'*/],
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: true,
@@ -53,5 +60,12 @@ module.exports = function(config) {
         concurrency: Infinity,
 
         coverageReporter: { type : 'html', dir : 'coverage/' }
-    })
+    };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
+
 };
